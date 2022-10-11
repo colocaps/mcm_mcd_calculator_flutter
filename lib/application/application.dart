0220f6459ccf6f64281045be263fcb97e2b023f7
colocaps <colocaps@gmail.com>
2022-10-11 13:08:02 -0300

    build home module

---
+++ lib/application/application.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../features/home_module.dart';
+
+class Application extends StatefulWidget {
+  const Application({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeModule.homeRoute,
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', ''),
+        Locale('en', ''),
+      ],
+      routes: _generateRoutes(),
+    );
+  }
+
+  Map<String, WidgetBuilder> _generateRoutes() {
+    return {};
+  }
+}
---
+++ lib/features/home_module.dart
@@ -0,0 +1,33 @@
+import 'package:core/core.dart';
+import 'package:flutter/material.dart';
+
+import 'home_module/pages/home_page.dart';
+
+abstract class HomeModule {
+  static late final NavigationManager _navigationManager;
+
+  static registerDependencies() {
+    _navigationManager =
+        InjectorContainer.instance.resolve<NavigationManager>();
+  }
+
+  static const String homeRoute = '/home';
+
+  static Map<String, WidgetBuilder> generateRoutes() {
+    return {
+      homeRoute: (context) => const HomePage(),
+    };
+  }
+
+  static Future<T?> navigateToHome<T, TArgs>(
+    BuildContext context,
+    TArgs args,
+  ) {
+    return _navigationManager.pushNamedAndRemoveUntil(
+      context,
+      homeRoute,
+      (homeRoute) => false,
+      args: args,
+    );
+  }
+}
